375522f3d7a4adc5bed822cbc33dbb55f846e61c
anandanshul <anshulanand194@gmail.com>
2026-04-25 15:41:16 +0000

    implement string, vector3 & array objects

--- main.c
+++ main.c
@@ -2,43 +2,81 @@
 #include "snekobject.h"
 #include <stdlib.h>
 
-munit_case(RUN, test_positive, {
-  snek_object_t *obj = new_snek_float(42);
-  assert_float(obj->data.v_float, ==, 42, "Must accept positive values");
+munit_case(RUN, test_array, {
+  // Create an array
+  snek_object_t *obj = new_snek_array(2);
 
+  // Create some objects to put in the array
+  // Can store objects, even though they have different types (just like Python)
+  snek_object_t *first = new_snek_string("First");
+  snek_object_t *second = new_snek_integer(3);
+
+  assert(snek_array_set(obj, 0, first));
+  assert(snek_array_set(obj, 1, second));
+
+  snek_object_t *retrieved_first = snek_array_get(obj, 0);
+  assert_not_null(retrieved_first, "Should find the first object");
+  assert_int(retrieved_first->kind, ==, STRING, "Should be a string");
+  assert_ptr(first, ==, retrieved_first, "Should be the same object");
+
+  snek_object_t *retrieved_second = snek_array_get(obj, 1);
+  assert_not_null(retrieved_second, "Should find the second object");
+  assert_int(retrieved_second->kind, ==, INTEGER, "Should be a integer");
+  assert_ptr(second, ==, retrieved_second, "Should be the same object");
+
+  free(first->data.v_string);
+  free(first);
+  free(second);
+  free(obj->data.v_array.elements);
   free(obj);
   assert(boot_all_freed());
 });
 
-munit_case(SUBMIT, test_zero, {
-  snek_object_t *obj = new_snek_float(0.0);
+munit_case(RUN, test_set_outside_bounds, {
+  // Create an array
+  snek_object_t *obj = new_snek_array(2);
 
-  assert_float(obj->kind, ==, FLOAT, "Must set type to FLOAT");
-  assert_float(obj->data.v_float, ==, 0.0, "Must accept 0.0");
+  snek_object_t *outside = new_snek_string("First");
 
+  // Inside of bound
+  assert(snek_array_set(obj, 1, outside));
+
+  // Outside of bound
+  assert_false(snek_array_set(obj, 100, outside));
+
+  // Free memory
+  free(outside->data.v_string);
+  free(outside);
+  free(obj->data.v_array.elements);
   free(obj);
   assert(boot_all_freed());
 });
 
-munit_case(SUBMIT, test_negative, {
-  snek_object_t *obj = new_snek_float(-5.0);
+munit_case(SUBMIT, test_get_outside_bounds, {
+  // Create an array
+  snek_object_t *obj = new_snek_array(1);
+  snek_object_t *first = new_snek_string("First");
+  assert(snek_array_set(obj, 0, first));
 
-  assert_float(obj->kind, ==, FLOAT, "Must set type to FLOAT");
-  assert_float(obj->data.v_float, ==, -5.0, "Must accept negative numbers");
+  // Outside of bound
+  assert_null(snek_array_get(obj, 1), "Should not access outside the array");
 
+  free(first->data.v_string);
+  free(first);
+  free(obj->data.v_array.elements);
   free(obj);
   assert(boot_all_freed());
 });
 
 int main() {
   MunitTest tests[] = {
-      munit_test("/positive", test_positive),
-      munit_test("/zero", test_zero),
-      munit_test("/negative", test_negative),
+      munit_test("/set_and_get", test_array),
+      munit_test("/set_outside", test_set_outside_bounds),
+      munit_test("/get_outside", test_get_outside_bounds),
       munit_null_test,
   };
 
-  MunitSuite suite = munit_suite("object-float", tests);
+  MunitSuite suite = munit_suite("object-array", tests);
 
   return munit_suite_main(&suite, NULL, 0, NULL);
 }
