5727f5ed302b0e54f98fbb45babfa13c69691921
anandanshul <anshulanand194@gmail.com>
2026-04-25 15:03:29 +0000

    implement int, float

--- main.c
+++ main.c
@@ -2,27 +2,43 @@
 #include "snekobject.h"
 #include <stdlib.h>
 
-munit_case(RUN, test_integer_constant,
-           { assert_int(INTEGER, ==, 0, "INTEGER is defined as 0"); });
-
-munit_case(RUN, test_integer_obj, {
-  snek_object_t *obj = malloc(sizeof(snek_object_t));
-  obj->kind = INTEGER;
-  obj->data.v_int = 0;
-  assert_int(obj->kind, ==, INTEGER, "must be INTEGER type");
-  assert_int(obj->data.v_int, ==, 0, "must equal zero");
+munit_case(RUN, test_positive, {
+  snek_object_t *obj = new_snek_float(42);
+  assert_float(obj->data.v_float, ==, 42, "Must accept positive values");
 
   free(obj);
+  assert(boot_all_freed());
+});
+
+munit_case(SUBMIT, test_zero, {
+  snek_object_t *obj = new_snek_float(0.0);
+
+  assert_float(obj->kind, ==, FLOAT, "Must set type to FLOAT");
+  assert_float(obj->data.v_float, ==, 0.0, "Must accept 0.0");
+
+  free(obj);
+  assert(boot_all_freed());
+});
+
+munit_case(SUBMIT, test_negative, {
+  snek_object_t *obj = new_snek_float(-5.0);
+
+  assert_float(obj->kind, ==, FLOAT, "Must set type to FLOAT");
+  assert_float(obj->data.v_float, ==, -5.0, "Must accept negative numbers");
+
+  free(obj);
+  assert(boot_all_freed());
 });
 
 int main() {
   MunitTest tests[] = {
-      munit_test("/integer_constant", test_integer_constant),
-      munit_test("/integer_obj", test_integer_obj),
+      munit_test("/positive", test_positive),
+      munit_test("/zero", test_zero),
+      munit_test("/negative", test_negative),
       munit_null_test,
   };
 
-  MunitSuite suite = munit_suite("object-integer-def", tests);
+  MunitSuite suite = munit_suite("object-float", tests);
 
   return munit_suite_main(&suite, NULL, 0, NULL);
 }
---
+++ snekobject.c
@@ -0,0 +1,25 @@
+#include "snekobject.h"
+#include <stdlib.h>
+
+snek_object_t *new_snek_float(float value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = FLOAT;
+  obj->data.v_float = value;
+  return obj;
+}
+
+
+snek_object_t *new_snek_integer(int value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = INTEGER;
+  obj->data.v_int = value;
+  return obj;
+}
